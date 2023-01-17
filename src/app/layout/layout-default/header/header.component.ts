cb7a33082752fdcc938119d29fba48ef56eec632
Merih Sakarya <merih.sakarya@sahabt.com>
2023-01-17 10:43:30 +0000

    email from user-context

--- src/app/layout/layout-default/header/header.component.ts
+++ src/app/layout/layout-default/header/header.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '@core/services/authentication.service';
+import { UserContextUtil } from '@shared/utils/user-context.util';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
+  user = UserContextUtil.getUserContext();
+
   constructor(private authenticationService: AuthenticationService) {}
 
   ngOnInit(): void {}
